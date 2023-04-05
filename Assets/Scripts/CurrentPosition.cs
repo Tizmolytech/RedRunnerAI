99e1cd4c9b6b2e4c8e91bec9f9a2f63bf198993b
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-04-05 12:11:07 +0200

    Added TMP to see position, velocity and acceleration.

---
+++ Assets/Scripts/CurrentPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using RedRunner.Characters;
+using UnityEngine.Networking;
+
+public class CurrentPosition : MonoBehaviour
+{
+    public TextMeshProUGUI printInfo;
+    public Character player;
+    private Vector2 previousVelocity;
+
+    void Start()
+    {
+        printInfo = GameObject.Find("Information Text").GetComponent<TextMeshProUGUI>();
+        player = GameObject.FindWithTag("Player").GetComponent<Character>();
+        previousVelocity = Vector2.zero;
+    }
+
+    void Update()
+    {
+        Vector2 currentVelocity = player.GetComponent<Rigidbody2D>().velocity;
+        Vector2 acceleration = (currentVelocity - previousVelocity) / Time.deltaTime;
+        previousVelocity = currentVelocity;
+
+        printInfo.text = "Current Position: " + player.transform.position + "\n" +
+        "Current Velocity: " + currentVelocity + "\n" +
+        "Current Acceleration: " + acceleration + "\n";
+    }
+}
+
