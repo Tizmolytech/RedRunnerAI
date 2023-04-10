042e6ef45a5775d1b707a2234640807be6cf0d6b
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-04-10 14:01:22 +0200

    Updated hitbox print format and sorted enemy list so the first enemy is the closest from RedRunner.

--- Assets/Scripts/CurrentPosition.cs
+++ Assets/Scripts/CurrentPosition.cs
@@ -6,7 +6,6 @@ using RedRunner.Characters;
 using RedRunner.Enemies;
 using RedRunner.Utilities;
 using RedRunner.TerrainGeneration;
-using System.ComponentModel;
 using System.Diagnostics;
 
 public class CurrentPosition : MonoBehaviour
@@ -37,15 +36,18 @@ public class CurrentPosition : MonoBehaviour
         Vector2 currentVelocity = player.GetComponent<Rigidbody2D>().velocity;
         Vector2 acceleration = (currentVelocity - previousVelocity) / Time.deltaTime;
         previousVelocity = currentVelocity;
+        Vector4 redRunnerHitBox = ComputeHitBox(player);
 
         printInfo.text = "Current Position: " + player.transform.position + "\n" +
         "Current Velocity: " + currentVelocity + "\n" +
         "Current Acceleration: " + acceleration + "\n" +
-        "Current Hitbox: [" + ComputeHitBox(player)[0] + ", " + ComputeHitBox(player)[1] + ", " + ComputeHitBox(player)[2] + ", " + ComputeHitBox(player)[3] + "]\n";
+        "Current Hitbox: [(" + redRunnerHitBox.x + ", " + redRunnerHitBox.y + ");(" + redRunnerHitBox.z + ", " + redRunnerHitBox.w + ")]\n";
 
         detectedEnemies.Clear();
 
         Enemy[] allEnemies = GameObject.FindObjectsOfType<Enemy>();
+        allEnemies = SortEnemiesByDistance(allEnemies);
+        
         foreach (Enemy e in allEnemies)
         {
             float enemyRadiusX = e.GetComponent<Collider2D>().bounds.extents.x;
@@ -64,8 +66,8 @@ public class CurrentPosition : MonoBehaviour
 
         for (int i = 0; i < detectedEnemies.Count; i++)
         {
-            string[] hitBox = ComputeHitBox(detectedEnemies[i]);
-            printInfo.text += detectedEnemies[i].GetType().Name + " " + ComputePosition(detectedEnemies[i]) + " [" + hitBox[0] + ", " + hitBox[1] + ", " + hitBox[2] + ", " + hitBox[3] + "]\n";
+            Vector4 hitBox = ComputeHitBox(detectedEnemies[i]);
+            printInfo.text += detectedEnemies[i].GetType().Name + " " + ComputePosition(detectedEnemies[i]) + " [(" + hitBox.x + ", " + hitBox.y + ");(" + hitBox.z + ", " + hitBox.w + ")]\n";
         }
     }
 
@@ -81,25 +83,44 @@ public class CurrentPosition : MonoBehaviour
             return Vector2.zero;
         }
     }
-
-    private string[] ComputeHitBox(UnityEngine.Component component)
+    
+    private Vector4 ComputeHitBox(UnityEngine.Component component)
     {
         if (component is Character || component is Enemy)
         {
             Vector2 edges = new Vector2(component.GetComponent<Collider2D>().bounds.extents.x, component.GetComponent<Collider2D>().bounds.extents.y);
-            string[] hitBox = new string[4];
-
-            hitBox[0] = (component.transform.position.x - edges.x).ToString("0.0");
-            hitBox[1] = (component.transform.position.y - edges.y).ToString("0.0");
-            hitBox[2] = (component.transform.position.x + edges.x).ToString("0.0");
-            hitBox[3] = (component.transform.position.y + edges.y).ToString("0.0");
+            Vector4 hitBox = new Vector4(component.transform.position.x - edges.x, component.transform.position.y - edges.y, component.transform.position.x + edges.x, component.transform.position.y + edges.y);
 
             return hitBox;
         }
         else
         {
             UnityEngine.Debug.LogError("Component for hitbox is not a Character or Enemy");
-            return null;
+            return Vector4.zero;
         }
+    }
+
+    private Enemy[] SortEnemiesByDistance(Enemy[] enemies)
+    {
+        Enemy[] sortedEnemies = new Enemy[enemies.Length];
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            sortedEnemies[i] = enemies[i];
+        }
+
+        for (int i = 0; i < sortedEnemies.Length; i++)
+        {
+            for (int j = i + 1; j < sortedEnemies.Length; j++)
+            {
+                if (Vector2.Distance(sortedEnemies[i].transform.position, player.transform.position) > Vector2.Distance(sortedEnemies[j].transform.position, player.transform.position))
+                {
+                    Enemy temp = sortedEnemies[i];
+                    sortedEnemies[i] = sortedEnemies[j];
+                    sortedEnemies[j] = temp;
+                }
+            }
+        }
+
+        return sortedEnemies;
     }
 }
