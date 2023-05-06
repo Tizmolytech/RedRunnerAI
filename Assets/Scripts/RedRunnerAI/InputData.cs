25c0718181a5f1431fcbdfef18019c8c72c00bda
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-05-06 17:08:58 +0200

    Updated InputData.

--- Assets/Scripts/RedRunnerAI/InputData.cs
+++ Assets/Scripts/RedRunnerAI/InputData.cs
@@ -2,34 +2,34 @@ using System.Collections.Generic;
 using RedRunner.Characters;
 using RedRunner.Enemies;
 using RedRunner.Collectables;
-using RedRunner.TerrainGeneration;
 using UnityEngine;
+using System.IO;
+using System;
 /**
- * Class to prepare data
- * Data is a grid of the screen and each cell represents an object such as an enemy, a player, a ground block, etc.
- * The goal of this class is to "prepare" the data so that it can be used by the neural network
- * The data looks like a grid with bytes representing the objects
+* Class to prepare data
+* Data is a grid of the screen and each cell represents an object such as an enemy, a player, a ground block, etc.
+* The goal of this class is to "prepare" the data so that it can be used by the neural network
+* The data looks like a grid with bytes representing the objects
 **/
 public class InputData : MonoBehaviour
 {
 	private byte[,] data;
-	private uint numberInputs;
     private Character player;
     private Vector2 previousVelocity;
     private Camera cam;
-    private Vector2 detectionRange;
+    private Vector2 detectionRange; 
     private Dictionary<Coin, Vector4> detectedCoins;
     private Dictionary<Enemy, Vector4> detectedEnemies;
-    private Dictionary<Block, Vector4> detectedBlocks;
-    private const byte smallestObject = 4; //Coin which is 0.4 square
-
+    private Dictionary<GameObject, Vector4> detectedGround;
+    private float smallestObject =1f;
+    private string filePath = "C:\\Users\\mathi\\OneDrive\\Bureau\\grid.txt";
 	void Start()
 	{
         player = GameObject.Find("RedRunner").GetComponent<Character>();
         previousVelocity = Vector2.zero;
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
         detectionRange = new Vector2(cam.orthographicSize * cam.aspect, cam.orthographicSize);
-        data = new byte[(int)detectionRange.x / smallestObject, (int)detectionRange.y / smallestObject];
+        data = new byte[Mathf.CeilToInt(2 * detectionRange.y / smallestObject), Mathf.CeilToInt(2 * detectionRange.x / smallestObject)];
     }
 
 	void Update()
@@ -37,12 +37,14 @@ public class InputData : MonoBehaviour
         Vector2 currentVelocity = player.GetComponent<Rigidbody2D>().velocity;
         previousVelocity = currentVelocity;
 
-        detectedCoins = detectObjects(FindObjectsOfType<Coin>());
-        detectedEnemies = detectObjects(FindObjectsOfType<Enemy>());
-        detectedBlocks = detectObjects(FindObjectsOfType<Block>());
+        detectedCoins = DetectObjects(FindObjectsOfType<Coin>());
+        detectedEnemies = DetectObjects(FindObjectsOfType<Enemy>());
+        detectedGround = DetectGround(GameObject.FindGameObjectsWithTag("Ground"));
+        ComputeGrid();
+        SaveGrid();
     }
 
-    private Dictionary<T, Vector4> detectObjects<T>(T[] allObjects) where T : MonoBehaviour
+    private Dictionary<T, Vector4> DetectObjects<T>(T[] allObjects) where T : MonoBehaviour
     {
         Dictionary<T, Vector4> result = new Dictionary<T, Vector4>();
 
@@ -59,6 +61,26 @@ public class InputData : MonoBehaviour
         return result;
     }
 
+    private Dictionary<GameObject, Vector4> DetectGround(GameObject[] allGrounds)
+    {
+        Dictionary<GameObject, Vector4> result = new Dictionary<GameObject, Vector4>();
+
+        foreach (GameObject obj in allGrounds)
+        {
+            if (obj.GetComponent<Collider2D>())
+            {
+                Vector4 hitBox = ComputeHitBox(obj.GetComponent<Collider2D>());
+
+                if (hitBox.z > cam.transform.position.x - detectionRange.x && hitBox.x < cam.transform.position.x + detectionRange.x && hitBox.w > cam.transform.position.y - detectionRange.y && hitBox.y < cam.transform.position.y + detectionRange.y)
+                {
+                    result.Add(obj, hitBox);
+                }
+            }
+        }
+
+        return result;
+    }
+
     private Vector4 ComputeHitBox(Component component)
     {
         Vector3 exts = component.GetComponent<Collider2D>().bounds.extents;
@@ -66,8 +88,100 @@ public class InputData : MonoBehaviour
         return new Vector4(component.transform.position.x - exts.x, component.transform.position.y - exts.y, component.transform.position.x + exts.x, component.transform.position.y + exts.y);
     }
 
-    private void computeGrid()
+    private void ComputeGrid()
     {
+        /**
+         * If in the cell, there is nothing, the value is 0
+         * If in the cell, there is an enemy, the value is 1
+         * If in the cell, there is a coin, the value is 2
+         * If in the cell, there is a ground block, the value is 3
+         * If in the cell, there is the player, the value is 4
+        **/
+        ResetGrid();
 
+        foreach (KeyValuePair<Coin, Vector4> coin in detectedCoins)
+        {
+            Vector4 gridPosition = CorrectPosition(new Vector4(coin.Value.x - cam.transform.position.x + detectionRange.x, coin.Value.y - cam.transform.position.y + detectionRange.y, coin.Value.z - cam.transform.position.x + detectionRange.x, coin.Value.w - cam.transform.position.y + detectionRange.y));
+
+            for (byte loopWidth = (byte)Math.Truncate(gridPosition.x); loopWidth < Math.Truncate(gridPosition.z) + 1; loopWidth++)
+            {
+                for (byte loopHeight = (byte)Math.Truncate(gridPosition.y); loopHeight < Math.Truncate(gridPosition.w) + 1; loopHeight++)
+                {
+                    data[loopHeight, loopWidth] = 2;
+                }
+            }
+        }
+
+        foreach (KeyValuePair<Enemy, Vector4> enemy in detectedEnemies)
+        {
+            Vector4 gridPosition = CorrectPosition(new Vector4(enemy.Value.x - cam.transform.position.x + detectionRange.x, enemy.Value.y - cam.transform.position.y + detectionRange.y, enemy.Value.z - cam.transform.position.x + detectionRange.x, enemy.Value.w - cam.transform.position.y + detectionRange.y));
+            
+            for (byte loopWidth = (byte)Math.Truncate(gridPosition.x); loopWidth < Math.Truncate(gridPosition.z) + 1; loopWidth++)
+            {
+                for (byte loopHeight = (byte)Math.Truncate(gridPosition.y); loopHeight < Math.Truncate(gridPosition.w) + 1; loopHeight++)
+                {
+                    data[loopHeight, loopWidth] = 1;
+                }
+            }
+        }
+
+        foreach (KeyValuePair<GameObject, Vector4> ground in detectedGround)
+        {
+            Vector4 gridPosition = CorrectPosition(new Vector4(ground.Value.x - cam.transform.position.x + detectionRange.x, ground.Value.y - cam.transform.position.y + detectionRange.y, ground.Value.z - cam.transform.position.x + detectionRange.x, ground.Value.w - cam.transform.position.y + detectionRange.y));
+            
+            for (byte loopWidth = (byte)Math.Truncate(gridPosition.x); loopWidth < Math.Truncate(gridPosition.z) + 1; loopWidth++)
+            {
+                for (byte loopHeight = (byte)Math.Truncate(gridPosition.y); loopHeight < Math.Truncate(gridPosition.w) + 1; loopHeight++)
+                {
+                    data[loopHeight, loopWidth] = 3;
+                }
+            }
+        }
+
+        Vector4 playerPosition = ComputeHitBox(player.GetComponent<Collider2D>());
+        playerPosition = CorrectPosition(new Vector4(playerPosition.x - cam.transform.position.x + detectionRange.x, playerPosition.y - cam.transform.position.y + detectionRange.y, playerPosition.z - cam.transform.position.x + detectionRange.x, playerPosition.w - cam.transform.position.y + detectionRange.y));
+        
+        for (byte loopWidth = (byte)Math.Truncate(playerPosition.x); loopWidth < Math.Truncate(playerPosition.z) + 1; loopWidth++)
+        {
+            for (byte loopHeight = (byte)Math.Truncate(playerPosition.y); loopHeight < Math.Truncate(playerPosition.w) + 1; loopHeight++)
+            {
+                data[loopHeight, loopWidth] = 4;
+            }
+        }
+    }
+
+    private void SaveGrid()
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            for (int loopWidth = data.GetLength(0) -1; loopWidth >= 0; loopWidth--)
+            {
+                for (int loopHeight = 0; loopHeight < data.GetLength(1); loopHeight++)
+                {
+                    writer.Write(data[loopWidth, loopHeight] + " ");
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+
+    private Vector4 CorrectPosition(Vector4 position)
+    {
+        position.x = Mathf.Clamp(position.x / smallestObject, 0, data.GetLength(1) - 1);
+        position.y = Mathf.Clamp(position.y / smallestObject, 0, data.GetLength(0) - 1);
+        position.z = Mathf.Clamp(position.z / smallestObject, 0, data.GetLength(1) - 1);
+        position.w = Mathf.Clamp(position.w / smallestObject, 0, data.GetLength(0) - 1);
+
+        return position;
+    }
+    private void ResetGrid()
+    {
+        for (ushort loopWidth = 0; loopWidth < data.GetLength(0); loopWidth++)
+        {
+            for (ushort loopHeight = 0; loopHeight < data.GetLength(1); loopHeight++)
+            {
+                data[loopWidth, loopHeight] = 0;
+            }
+        }
     }
 }
