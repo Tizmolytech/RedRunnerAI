29269c85c83466198ad4898b4c76a33e7345a5c8
MathisCPolytech <mathis.charpentier-2@etu.univ-tours.fr>
2023-06-06 11:29:30 +0200

    Updated grid

--- Assets/Scripts/RedRunnerAI/InputData.cs
+++ Assets/Scripts/RedRunnerAI/InputData.cs
@@ -1,47 +1,46 @@
 using System.Collections.Generic;
-using RedRunner.Characters;
-using RedRunner.Enemies;
-using RedRunner.Collectables;
-using UnityEngine;
 using System.IO;
 using System;
-/**
-* Class to prepare data
-* Data is a grid of the screen and each cell represents an object such as an enemy, a player, a ground block, etc.
-* The goal of this class is to "prepare" the data so that it can be used by the neural network
-* The data looks like a grid with bytes representing the objects
-**/
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using RedRunner.Enemies;
+using RedRunner.Characters;
+using UnityEngine.UI;
+
 public class InputData : MonoBehaviour
 {
-	private byte[,] data;
+    private byte[,] data;
     private Character player;
-    private Vector2 previousVelocity;
     private Camera cam;
-    private Vector2 detectionRange; 
-    private Dictionary<Coin, Vector4> detectedCoins;
+    private Vector2 detectionRange;
     private Dictionary<Enemy, Vector4> detectedEnemies;
     private Dictionary<GameObject, Vector4> detectedGround;
-    private float smallestObject =1f;
-    private string filePath = Environment.GetEnvironmentVariable("USERPROFILE") + "\\OneDrive\\Bureau\\grid.txt";
-	void Start()
-	{
+    private RawImage gridImage;
+    private readonly float smallestObjectX = 1.02f; // width of semi circular saw
+    private readonly float smallestObjectY = 0.86f; // height of a spike up
+    private readonly string filePath = Environment.GetEnvironmentVariable("USERPROFILE") + "\\OneDrive\\Bureau\\grid.txt";
+
+    public Color[] colorMap = new Color[4]; // Array of colors corresponding to different object types
+
+    private void Start()
+    {
         player = GameObject.Find("RedRunner").GetComponent<Character>();
-        previousVelocity = Vector2.zero;
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        gridImage = GameObject.Find("Minimap").GetComponent<RawImage>();
         detectionRange = new Vector2(cam.orthographicSize * cam.aspect, cam.orthographicSize);
-        data = new byte[Mathf.CeilToInt(2 * detectionRange.y / smallestObject), Mathf.CeilToInt(2 * detectionRange.x / smallestObject)];
+        data = InitDataSize();
+        colorMap[0] = new Color(1, 1, 1, 0.5f);
+        colorMap[1] = Color.red;
+        colorMap[2] = Color.green;
+        colorMap[3] = Color.blue;
     }
 
-	void Update()
-	{
-        Vector2 currentVelocity = player.GetComponent<Rigidbody2D>().velocity;
-        previousVelocity = currentVelocity;
-
-        detectedCoins = DetectObjects(FindObjectsOfType<Coin>());
+    private void Update()
+    {
         detectedEnemies = DetectObjects(FindObjectsOfType<Enemy>());
         detectedGround = DetectGround(GameObject.FindGameObjectsWithTag("Ground"));
         ComputeGrid();
-        SaveGrid();
+        DisplayGrid();
     }
 
     private Dictionary<T, Vector4> DetectObjects<T>(T[] allObjects) where T : MonoBehaviour
@@ -90,69 +89,59 @@ public class InputData : MonoBehaviour
 
     private void ComputeGrid()
     {
-        /**
-         * If in the cell, there is nothing, the value is 0
-         * If in the cell, there is an enemy, the value is 1
-         * If in the cell, there is a coin, the value is 2
-         * If in the cell, there is a ground block, the value is 3
-         * If in the cell, there is the player, the value is 4
-        **/
         ResetGrid();
 
-        foreach (KeyValuePair<Coin, Vector4> coin in detectedCoins)
-        {
-            FillObjectsInGrid(CorrectPosition(new Vector4(coin.Value.x - cam.transform.position.x + detectionRange.x, coin.Value.y - cam.transform.position.y + detectionRange.y, coin.Value.z - cam.transform.position.x + detectionRange.x, coin.Value.w - cam.transform.position.y + detectionRange.y)), 2);
-        }
-
         foreach (KeyValuePair<Enemy, Vector4> enemy in detectedEnemies)
-        {            
-            FillObjectsInGrid(CorrectPosition(new Vector4(enemy.Value.x - cam.transform.position.x + detectionRange.x, enemy.Value.y - cam.transform.position.y + detectionRange.y, enemy.Value.z - cam.transform.position.x + detectionRange.x, enemy.Value.w - cam.transform.position.y + detectionRange.y)), 1);
+        {
+            ComputePositionInGrid(enemy.Value, 1);
         }
 
         foreach (KeyValuePair<GameObject, Vector4> ground in detectedGround)
         {
-            FillObjectsInGrid(CorrectPosition(new Vector4(ground.Value.x - cam.transform.position.x + detectionRange.x, ground.Value.y - cam.transform.position.y + detectionRange.y, ground.Value.z - cam.transform.position.x + detectionRange.x, ground.Value.w - cam.transform.position.y + detectionRange.y)), 3);
+            ComputePositionInGrid(ground.Value, 2);
         }
 
         Vector4 playerPosition = ComputeHitBox(player.GetComponent<Collider2D>());
-        FillObjectsInGrid(CorrectPosition(new Vector4(playerPosition.x - cam.transform.position.x + detectionRange.x, playerPosition.y - cam.transform.position.y + detectionRange.y, playerPosition.z - cam.transform.position.x + detectionRange.x, playerPosition.w - cam.transform.position.y + detectionRange.y)), 4);
+        ComputePositionInGrid(playerPosition, 3);
+
+        DisplayGrid();
     }
 
-    private void SaveGrid()
+    private void ComputePositionInGrid(Vector4 hitBox, byte objectType)
+    {
+        Vector4 gridPosition = CorrectPosition(CorrectValue(new Vector4((hitBox.x - cam.transform.position.x + detectionRange.x) / smallestObjectX,
+            (hitBox.y - cam.transform.position.y + detectionRange.y) / smallestObjectY, (hitBox.z - cam.transform.position.x + detectionRange.x)
+            / smallestObjectX, (hitBox.w - cam.transform.position.y + detectionRange.y) / smallestObjectY)));
+
+        for (int i = (int)gridPosition.y; i < (int)gridPosition.w; i++)
+        {
+            for (int j = (int)gridPosition.x; j < (int)gridPosition.z; j++)
+            {
+                data[i, j] = objectType;
+            }
+        }
+    }
+
+    private Vector4 CorrectPosition(Vector4 hitBox)
+    {
+        return new Vector4(CustomRound(hitBox.x), CustomRound(hitBox.y), CustomRound(hitBox.z), CustomRound(hitBox.w));
+    }
+
+    private void SaveGrid(byte[,] dataGrid)
     {
         using (StreamWriter writer = new StreamWriter(filePath))
         {
-            for (int loopWidth = data.GetLength(0) -1; loopWidth >= 0; loopWidth--)
+            for (int loopWidth = dataGrid.GetLength(0) - 1; loopWidth >= 0; loopWidth--)
             {
-                for (int loopHeight = 0; loopHeight < data.GetLength(1); loopHeight++)
+                for (int loopHeight = 0; loopHeight < dataGrid.GetLength(1); loopHeight++)
                 {
-                    writer.Write(data[loopWidth, loopHeight] + " ");
+                    writer.Write(dataGrid[loopWidth, loopHeight] + " ");
                 }
                 writer.WriteLine();
             }
         }
     }
 
-    private void FillObjectsInGrid(Vector4 position, byte objectType)
-    {
-        for (byte loopWidth = (byte)Math.Truncate(position.x); loopWidth < Math.Truncate(position.z) + 1; loopWidth++)
-        {
-            for (byte loopHeight = (byte)Math.Truncate(position.y); loopHeight < Math.Truncate(position.w) + 1; loopHeight++)
-            {
-                data[loopHeight, loopWidth] = objectType;
-            }
-        }
-    }
-
-    private Vector4 CorrectPosition(Vector4 position)
-    {
-        position.x = Mathf.Clamp(position.x / smallestObject, 0, data.GetLength(1) - 1);
-        position.y = Mathf.Clamp(position.y / smallestObject, 0, data.GetLength(0) - 1);
-        position.z = Mathf.Clamp(position.z / smallestObject, 0, data.GetLength(1) - 1);
-        position.w = Mathf.Clamp(position.w / smallestObject, 0, data.GetLength(0) - 1);
-
-        return position;
-    }
     private void ResetGrid()
     {
         for (ushort loopWidth = 0; loopWidth < data.GetLength(0); loopWidth++)
@@ -162,5 +151,75 @@ public class InputData : MonoBehaviour
                 data[loopWidth, loopHeight] = 0;
             }
         }
+    }
+
+    private byte[,] InitDataSize()
+    {
+        float rawSizeX = (2 * detectionRange.x) / smallestObjectX;
+        float rawSizeY = (2 * detectionRange.y) / smallestObjectY;
+
+        return new byte[CustomRound(rawSizeY), CustomRound(rawSizeX)];
+    }
+
+    private Vector4 CorrectValue(Vector4 hitBox)    
+    {
+        if (hitBox.x < 0)
+            hitBox.x = 0;
+        if (hitBox.y < 0)
+            hitBox.y = 0;
+        if (hitBox.z < 0)
+            hitBox.z = 0;
+        if (hitBox.w < 0)
+            hitBox.w = 0;
+        if (hitBox.x >= data.GetLength(1))
+            hitBox.x = data.GetLength(1) - 1;
+        if (hitBox.y >= data.GetLength(0))
+            hitBox.y = data.GetLength(0) - 1;
+        if (hitBox.z >= data.GetLength(1))
+            hitBox.z = data.GetLength(1) - 1;
+        if (hitBox.w >= data.GetLength(0))
+            hitBox.w = data.GetLength(0) - 1;
+
+        return hitBox;
+    }
+
+    private byte CustomRound(float value)
+    {
+        float result = (float)(value - Math.Truncate(value));
+
+        if (result >= 0 && result < 0.5)
+        {
+            value = (byte)Math.Truncate(value);
+        }
+        else if (result >= 0.5 && result < 1)
+        {
+            value = (byte)Math.Truncate(value) + 1;
+        }
+
+        return (byte)value;
+    }
+
+    private void DisplayGrid()
+    {
+        int width = data.GetLength(1) - 1;
+        int height = data.GetLength(0) - 1;
+
+        Texture2D texture = new(width, height);
+        texture.filterMode = FilterMode.Point;
+
+    
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                byte objectType = data[y, x];
+                Color color = colorMap[objectType];
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+
+        gridImage.texture = texture;
     }
 }
